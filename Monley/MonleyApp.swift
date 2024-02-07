aaa443dcc2cb0534664b760202b0e797dc1ff0d2
Artyom Vlasov <mr.twago@gmail.com>
2024-02-06 20:40:14 -0700

    Initial Commit

---
+++ Monley/MonleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonleyApp.swift
+//  Monley
+//
+//  Created by Artyom Vlasov on 2024-02-06.
+//
+
+import SwiftUI
+
+@main
+struct MonleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
